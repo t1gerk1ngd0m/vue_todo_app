dad78dc5a8fe82d50e673069e99a7ca39da2c873
Taiga Miwa <taiga.miwa888@gmail.com>
2019-09-10 18:49:45 +0900

    軽微な修正

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -16,7 +16,7 @@ class Api::TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
-      render :show, status: :updated
+      render :show, status: :ok
     else
       render json: @task.errors, status: :unprocessable_entity
     end
