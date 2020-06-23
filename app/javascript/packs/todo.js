fdb62c4d320ad3e72d8d24a7b48055d933f04833
Taiga Miwa <taiga.miwa888@gmail.com>
2020-06-23 18:39:04 +0900

    apolloの導入

--- app/javascript/packs/todo.js
+++ app/javascript/packs/todo.js
@@ -1,13 +1,25 @@
 import Vue from 'vue/dist/vue.esm.js'
 import axios from "axios";
 import VueAxiosPlugin from "./plugins/vue-axios";
-import Router from './router/router'
+import router from './router/router'
+import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost'
 import Header from './components/header.vue'
 
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+Vue.use(VueApollo)
 Vue.use(VueAxiosPlugin, { axios: axios })
 
 var app = new Vue({
-  router: Router,
+  router,
+  apolloProvider,
   el: '#app',
   components: {
     'navbar': Header,
